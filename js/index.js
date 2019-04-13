7e05881dde8ca44bbffd7c273b301d43a2cff9b3
Jeremy Morales <jeremy.morales@gmail.com>
2019-04-13 11:23:25 -0700

    added nav mouseover + dblclick toggling

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
 // Your code goes here
+const links = document.querySelectorAll('.nav-link');
+console.log(links[0]);
+links.forEach(function(i) {
+    i.addEventListener('mouseover', (e) => e.target.style.color = 'green');
+    i.addEventListener('mouseout', (e) => e.target.style.color = '');
+})
+
+const dbl = document.querySelector('body');
+
+const testing = document.getElementsByTagName('head');
+console.log(testing);
+
+function createClass(name, rules) {
+    var style = document.createElement('style');
+    style.type = 'text/css';
+    document.getElementsByTagName('head')[0].appendChild(style);
+    if (!(style.sheet || {}).insertRule)
+        (style.styleSheet || style.sheet).addRule(name, rules);
+    else
+        style.sheet.insertRule(name + "{" + rules + "}", 0);
+}
+createClass('.switched', "background-color: #212529; color: white;");
+const navCont = document.querySelector(mainNavigation);
+console.log(navCont);
+
+dbl.addEventListener("dblclick", function () {
+
+    document.body.classList.toggle("switched");
+
+});
+    
